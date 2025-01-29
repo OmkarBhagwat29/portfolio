bf51a01266e74e005aa4f202a4f588925f1d0f73
Omkar Bhagwat <ombhagwat29@gmail.com>
2025-01-30 00:54:49 +0100

    url modified

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: isProd ? "export" : undefined,
+
   images: { unoptimized: true },
-  trailingSlash: true, // Required for GitHub Pages routing
-  basePath: isProd ? "/portfolio" : "", // Your repository name
-  assetPrefix: isProd ? "/portfolio" : "", // Match basePath with trailing slash
 
   // Redirect configuration (if you still need this)
   async redirects() {
