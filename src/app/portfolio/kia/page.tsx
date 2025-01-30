92208f2bfd3e112cf6fbad4f50d6620037bcd3e2
Omkar Bhagwat <ombhagwat29@gmail.com>
2025-01-30 01:17:30 +0100

    api route added

--- src/app/portfolio/kia/page.tsx
+++ src/app/portfolio/kia/page.tsx
@@ -1,21 +1,8 @@
+"use client";
 import React from "react";
-
-// import fs from "fs";
-// import path from "path";
 import Kia from "./Kia";
 
 const page = () => {
-  // const directory = "images/kia";
-  // const publicDirectory = path.join(process.cwd(), "public", directory);
-  // // console.log(publicDirectory);
-  // const images = fs
-  //   .readdirSync(publicDirectory)
-  //   .filter((file) =>
-  //     [".jpg", ".jpeg", ".png", ".webp"].includes(
-  //       path.extname(file).toLowerCase()
-  //     )
-  //   );
-
   return (
     <>
       <Kia />
